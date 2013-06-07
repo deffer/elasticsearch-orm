91237624d91704848474d7e4a6119bcc2343e1c8
Irina Benediktovich <i.benediktovich@auckland.ac.nz>
2013-06-07 03:49:40 +0000

    initial uoa-search-api commit

---
+++ src/main/java/nz/ac/auckland/search/IndexedField.java
@@ -0,0 +1,15 @@
+package nz.ac.auckland.search;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface IndexedField {
+
+    String indexFieldName();
+    boolean markupPresent() default false;
+
+}
