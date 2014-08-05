be4ebac31afee471b5b1e50d04d5fd20e24bff25
deffer <irina.benediktovich@gmail.com>
2014-08-06 00:37:22 +1200

    Better test subject

--- src/test/groovy/nz/ac/auckland/search/Person.groovy
+++ src/test/groovy/nz/ac/auckland/search/Person.groovy
@@ -5,86 +5,71 @@ import nz.ac.auckland.search.analysis.Filter;
 
 import java.util.*;
 
-@IndexedDocument(indexDocumentName='person',
-    analyzers = [
-        @Analyzer(name = 'snowball_english', type = 'snowball',
-            params = [@VariableParameter(name = 'language', value = 'English')]),
-        @Analyzer(name = 'snowball_spanish', type = 'snowball',
-            params = [@VariableParameter(name = 'language', value = 'Spanish')]),
-        @Analyzer(name = 'phonetic',
-            tokenizerName = 'whitespace',
-            filterNames = ['trim', 'lowercase', 'asciifolding', 'stop', 'unique', 'custom_metaphone'])],
-    filters = [
-        @Filter(name = 'custom_metaphone', type = 'phonetic',
-            params = [
-                @VariableParameter(name = 'encoder', value = 'metaphone'),
-                @VariableParameter(name = 'replace', value = 'false')] )
-])
+@IndexedDocument(indexDocumentName = 'person',
+		analyzers = [
+		@Analyzer(name = 'snowball_english', type = 'snowball', properties = 'language=English'),
+		@Analyzer(name = 'snowball_spanish', type = 'snowball', properties = 'language=Spanish'),
+		@Analyzer(name = 'phonetic',
+				tokenizerName = 'whitespace',
+				filterNames = ['trim', 'lowercase', 'asciifolding', 'stop', 'unique', 'custom_metaphone'])],
+		filters = [
+		@Filter(name = 'custom_metaphone', type = 'phonetic',properties = 'encoder=metaphone, replace=false')
+		])
 class Person {
-    Long id = 8
-    String field1 = "abc";
-    Set<Long> favoriteNumbers = [4,7,13,37,66,99] as Set
+	Long id = 8
 
-    @IndexedField(indexFieldName = 'age')
-    private Long annotatedField = 29  // private field without getter
+	Set<Long> favoriteNumbers = [4, 7, 13, 37, 66, 99] as Set
 
-    @IndexedField(indexFieldName = 'descrHTML')
-    private String descrHTML = "<br>Some text</br>";
+	String title = "Mrs";
 
-    @IndexedField(indexFieldName = 'descrTXT', markupPresent =  true)
-    String getDescr(){
-        return descrHTML
-    }
+	@IndexedField(indexFieldName = "personid")
+	public Long getId() { return id; } // getter for existing field
 
-    @IndexedField(indexFieldName="defaultValue")
-    String defaultValueField = "."; // getter returns different value
+	@IndexedField(indexFieldName = 'age')
+	private Long currentAge = 29  // private field without getter
 
-    private String getDefaultValueField(){
-        return "Lazy loaded value";
-    }
+	@IndexedField(indexFieldName = "defaultValue")
+	String defaultValueField = "."; // field with getter returning different value
 
-    @IndexedField(indexFieldName="duplication")
-    String duplication = "duplication field";
+	private String getDefaultValueField() {
+		return "Lazy loaded value";
+	}
 
-    @IndexedField(indexFieldName="duplication")
-    String getDuplication(){
-        return "duplication method";
-    }
+	@IndexedField(indexFieldName = "names")
+	String firstName = "Anna";
 
-    @IndexedField(indexFieldName = "id")
-    public Long getId(){return id;}
+	@IndexedField(indexFieldName = "names")
+	String lastName = "Torrez"
 
-    @IndexedField(indexFieldName = 'favoriteNumbers')
-    Set<Long> getFavoriteNumbers() {return favoriteNumbers}
+	@IndexedField(indexFieldName = "names")
+	public String getMiddleName() {
+		return "Maria";
+	}
 
-    @IndexedField(indexFieldName = 'favoriteNumbers')
-    List<Long> addSomethingToFavNumbers(){
-        return [111,222,333]
-    }
 
-    @IndexedField(indexFieldName = "h")
-    String getABC(){
-        return field1;
-    }
+	@IndexedField(indexFieldName = 'favorites')
+	Set<Long> getFavoriteNumbers() { return favoriteNumbers }
 
-    @IndexedField(indexFieldName = "h")
-    String getBCD(){
-        return "bcd";
-    }
+	@IndexedField(indexFieldName = 'favorites')
+	List<Long> additionalFavNumbers() {
+		return [111, 222, 333]
+	}
 
-    @IndexedField(indexFieldName = "h")
-    String notGetter(){
-        return "cdf";
-    }
+	@IndexedField(indexFieldName = "privateMethod")
+	private String currentDepartment() {
+		return "Science";
+	}
 
-    @IndexedField(indexFieldName = "privateMethod")
-    private String privateMethod(){
-        return field1;
-    }
+	@IndexedField(indexFieldName = "excluded")
+	Long methodWithArgs(int something) {
+		return new Long(888);
+	}
 
-    @IndexedField(indexFieldName = "excluded")
-    Long methodWithArgs(int something){
-        return new Long(888);
-    }
+	@IndexedField(indexFieldName = 'descrHTML')
+	private String descrHTML = "<br>Some text</br>";
 
+	@IndexedField(indexFieldName = 'descrTXT', markupPresent = true)
+	String getDescr() {
+		return descrHTML
+	}
 }
